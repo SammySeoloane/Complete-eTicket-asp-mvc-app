a5f35d523ebeafcc4e4b2622f007dc238cfcbcbc
TSeoloane <TSeoloane@PYRJHBNB37>
2023-02-21 13:16:55 +0200

    Add project files.

---
+++ eTicketSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eTicketSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
